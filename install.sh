3565e35e6eda984832c452d81c0e44dca8668d6f
Miguel Fuertes <hkfuertes@gmail.com>
2024-02-14 23:47:34 +0100

    working

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-sudo apt install -y cmake \
+sudo apt install -y cmake build-essential \
                      libavahi-compat-libdnssd-dev libplist-dev libssl-dev \
-                     build-essential\
-                     pkg-config \
-                     git libraspberrypi-dev libraspberrypi-doc
+                     pkg-config git \
+                     libraspberrypi-dev libraspberrypi-doc
 
-sudo apt install -y libssl-dev \
-    libplist-dev \
-    libavahi-compat-libdnssd-dev \
-    libgstreamer1.0-dev \
+sudo apt install -y libgstreamer1.0-dev \
     libgstreamer-plugins-base1.0-dev \
     gstreamer1.0-libav \
     gstreamer1.0-plugins-good \
@@ -17,8 +13,8 @@ sudo apt install -y libssl-dev \
     pulseaudio
 
 git clone https://github.com/FDH2/UxPlay && cd UxPlay
-export CFLAGS -D_FILE_OFFSET_BITS=64
-export CXXFLAGS -D_FILE_OFFSET_BITS=64
+export CFLAGS="-D_FILE_OFFSET_BITS=64"
+export CXXFLAGS="-D_FILE_OFFSET_BITS=64"
 
 # run cmake twice (weird bug)
 mkdir build && cd build && cmake ../ || true && \
