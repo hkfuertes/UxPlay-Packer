45a7ce6782cb0e5d4988530f5bbda14a7bd4c071
Miguel Fuertes <hkfuertes@gmail.com>
2024-02-14 22:51:21 +0100

    changes

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt install -y cmake \
+                     libavahi-compat-libdnssd-dev libplist-dev libssl-dev \
+                     build-essential\
+                     pkg-config \
+                     git
+
+sudo apt install -y libraspberrypi-dev libraspberrypi-doc
+
+sudo apt install -y libssl-dev \
+    libplist-dev \
+    libavahi-compat-libdnssd-dev \
+    libgstreamer1.0-dev \
+    libgstreamer-plugins-base1.0-dev \
+    gstreamer1.0-libav \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-bad \
+    pulseaudio
+
+git clone https://github.com/FDH2/UxPlay && cd UxPlay
+export CFLAGS -D_FILE_OFFSET_BITS=64
+export CXXFLAGS -D_FILE_OFFSET_BITS=64
+
+# run cmake twice (weird bug)
+mkdir build && cd build && cmake ../ || true && \
+    cmake .. -DCMAKE_CXX_FLAGS="-O3" -DCMAKE_C_FLAGS="-O3" &&\
+    make -j$(nproc) &&\
+    make install
